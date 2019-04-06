842670bbb5c3eee86e1f9f07bde63755346d1dac
AmrDeveloper <Amr96@programmer.net>
2019-04-06 05:40:06 +0200

    Create method to return current formatted date and time

--- src/astro/utils/DateUtils.java
+++ src/astro/utils/DateUtils.java
@@ -1,7 +1,14 @@
 package astro.utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DateUtils {
 
     private static final String DATE_TIME_FORMAT = "HH:mm:ss dd-MM-yyyy";
+    private static final SimpleDateFormat sDateFormat = new SimpleDateFormat(DATE_TIME_FORMAT);
 
+    public static String getCurrentDate(){
+        return sDateFormat.format(new Date());
+    }
 }
