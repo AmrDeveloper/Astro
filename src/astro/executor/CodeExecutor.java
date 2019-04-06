895239b2fc242353e339c1c201b6cd1853f4c59a
AmrDeveloper <Amr96@programmer.net>
2019-04-06 05:41:05 +0200

    Use DateUtils class in CodeExecutor to be more clean and abstract

--- src/astro/executor/CodeExecutor.java
+++ src/astro/executor/CodeExecutor.java
@@ -1,5 +1,6 @@
 package astro.executor;
 
+import astro.utils.DateUtils;
 import javafx.scene.control.TextArea;
 
 import java.io.File;
@@ -8,25 +9,25 @@ import java.util.Objects;
 
 public class CodeExecutor {
 
-    public static void execute(File source, TextArea outputArea){
+    public static void execute(File source, TextArea outputArea) {
         if (Objects.nonNull(source)) {
             Thread executeThread = new Thread(() -> {
                 new Executor().executeJava(source, new OnExecuteListener() {
                     @Override
                     public void onCompileFailure() {
-                        String message = "Compile is Failure\n" + new Date().toString();
+                        String message = "Compile is Failure\n" + DateUtils.getCurrentDate();
                         outputArea.setText(message);
                     }
 
                     @Override
                     public void onExecutorFailure() {
-                        String message = "Build is Failure\n" + new Date().toString();
+                        String message = "Build is Failure\n" + DateUtils.getCurrentDate();
                         outputArea.setText(message);
                     }
 
                     @Override
                     public void onExecutorSuccess(String output) {
-                        String message = "Build is Success\n" + new Date().toString() + "\n" .concat(output);
+                        String message = "Build is Success\n" + DateUtils.getCurrentDate() + "\n".concat(output);
                         outputArea.setText(message);
                     }
                 });
