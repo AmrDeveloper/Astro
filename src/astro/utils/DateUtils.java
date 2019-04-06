7a4fd4eef6435666dbeeff512551c39866e312e2
AmrDeveloper <Amr96@programmer.net>
2019-04-06 05:36:14 +0200

    Fix Format var name

--- src/astro/utils/DateUtils.java
+++ src/astro/utils/DateUtils.java
@@ -2,6 +2,6 @@ package astro.utils;
 
 public class DateUtils {
 
-    private static final String DATE_TIME_FORAMT = "HH:mm:ss dd-MM-yyyy";
+    private static final String DATE_TIME_FORMAT = "HH:mm:ss dd-MM-yyyy";
 
 }
