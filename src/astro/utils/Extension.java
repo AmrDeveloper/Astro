37a897ad2bea4679d1d1fea404ce2d2a6362e662
AmrDeveloper <Amr96@programmer.net>
2019-04-13 18:23:50 +0200

    Create Extension Class to store all files types

---
+++ src/astro/utils/Extension.java
@@ -0,0 +1,9 @@
+package astro.utils;
+
+public class Extension {
+    public static final String JAVA = ".JAVA";
+    public static final String CSS = ".css";
+
+    public static final String TEXT = ".txt";
+    public static final String MD = ".md";
+}
